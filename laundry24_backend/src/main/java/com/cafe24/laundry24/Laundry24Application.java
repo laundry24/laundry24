2c7c6672f066dbb40a0b2ad3bb4e3ef858320026
bgjo <lucy74310@gmail.com>
2020-04-18 01:32:40 +0900

    initiate laundry24 project

---
+++ laundry24_backend/src/main/java/com/cafe24/laundry24/Laundry24Application.java
@@ -0,0 +1,13 @@
+package com.cafe24.laundry24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laundry24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laundry24Application.class, args);
+    }
+
+}
---
+++ laundry24_backend/src/test/java/com/cafe24/laundry24/Laundry24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe24.laundry24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laundry24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
